4c1ef329aab009f9e80b350b728eba80e550ed42
Mario Jason Braganza <jason@janusworx.com>
2020-09-09 11:30:07 +0530

    Progress for 2020/09/09. Have decided to log in public. Read log.md for details
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

---
+++ log.md
@@ -0,0 +1,29 @@
+### 2020/08/09  
+Early start today.  
+Hope brains work.  
+
+made a lot of progress
+got a url, made sure there was only one url on the command line, sanitised the url, reached out on the web for it and put it into an object.  
+more tomorrow
+
+### 2020/09/08  
+Missed a day.  
+Trying to make up for it, by working a bit extra today.  
+could not get anything done, even after an hour.
+
+
+### 2020/09/04  
+Came up with the bones of this project today.  
+  
+Spent a lot of time today figuring out whether I could use the Requests library in my little project or not.  
+I want mjb-get-img to be MIT licensed, while Requests is Apache licensed.  
+After quite a bit of research online, I think I am in the clear.  
+If it transpires that I cannot in the future, I’ll figure out a way then.  
+
+I couldn’t think of a good name, so mjb-get-img (m-j-b get i-m-g), it is.  
+
+Found a handy README generator at [Make a Readme](https://www.makeareadme.com/).  
+Found lots of license clarity and boilerplate at [Choose a License](https://choosealicense.com/).  
+
+
+
--- mjbgetimg.py
+++ mjbgetimg.py
@@ -4,14 +4,25 @@ from that url/webpage (https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
 
 steps i think i need to do
 
-1. get a url from the command line. ensure only one site comes in at at time (for now)
-2. sanitise it? check for basic errors
+1. get a url from the command line. ensure only one site comes in at at time (for now) 
+- done!
+
+2. sanitise the url? check for basic errors 
+- managed to do basic sanity checks using a regex from an old django validator.
+https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
+- done
+
 3. use requests to get the web content
+
 4. use bs4 to check for image links
+
 5. create an images subfolder. if one exists, use it
+
 6. download the images and save them to the images subfolder
+
 """
 
+import re
 import sys
 
 import requests
@@ -24,8 +35,50 @@ def main():
     elif len(sys.argv) == 1:
         print("Please enter a webpage link, to get images from.")
     else:
-        print(f"number of command line arguments given: {len(sys.argv)}")
-        print(f"Arguments are: {sys.argv=}")
+        raw_url = sys.argv[1]
+        if sanitise_url(raw_url):
+            raw_page = get_page_from_requests(raw_url)
+            if raw_page:
+                print(raw_page.status_code)
+            else:
+                print("Uh-oh, something is wrong.")
+
+        else:
+            print(
+                f"{raw_url} is not a well formed domain name.\nPlease enter a valid url."
+            )
+
+
+def sanitise_url(raw_url):
+    """
+    Validates urls using an old django validator from https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
+
+    TODO someday - use the new django validator
+
+    :param raw_url: a url string
+    :type raw_url: string
+    :return: true or false
+    :rtype: boolean
+    """
+    regex = re.compile(
+        r"^(?:http|ftp)s?://"  # http:// or https://
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+        r"localhost|"  # localhost...
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+        r"(?::\d+)?"  # optional port
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+
+    return re.match(regex, raw_url)
+
+
+def get_page_from_requests(raw_url):
+    try:
+        r = requests.get(raw_url)
+        return r
+    except:
+        return None
 
 
 if __name__ == "__main__":
